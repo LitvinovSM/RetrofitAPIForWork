50835804f724ca4893ff8ce19589e7dddf377700
Сергей <litvinov.qa@yandex.ru>
2022-10-12 16:33:06 +0300

    error dto

---
+++ src/main/java/mainLogic/DTO/error/Error.java
@@ -0,0 +1,10 @@
+﻿package mainLogic.DTO.error;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Error {
+    @JsonProperty("error")
+    String error;
+}
